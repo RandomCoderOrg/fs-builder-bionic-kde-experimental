88b3e1eac93faa6d9b3da6c6061ba59d5ef74bfa
User0two <87519907+User0two@users.noreply.github.com>
2021-07-16 17:13:47 +0530

    Update stage3.sh

--- payload/stage3.sh
+++ payload/stage3.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 #TODO: KDE installation script goes here.
-apt-get update && apt upgrade -y
-apt-mark hold rsyslog
-apt install sudo -y
-apt install apt-utils -y
-apt install software-properties-common -y
-add-apt-repository ppa:kubuntu-ppa/backports -y
-apt update 
-apt install kubuntu-desktop -y && apt install tigervnc-standalone-server -y
-apt install plasma-pa -y
-apt install plasma-widgets-addons kubuntu-wallpapers-bionic -y
-apt install latte-dock -y
-apt update && sudo apt upgrade -y
+apt-get update && apt upgrade -y || true
+apt-mark hold rsyslog || true
+apt install sudo -y || true
+apt install apt-utils -y || true
+apt install software-properties-common -y || true
+add-apt-repository ppa:kubuntu-ppa/backports -y || true
+apt update || true
+apt install kubuntu-desktop -y && apt install tigervnc-standalone-server -y || true
+apt install plasma-pa -y || true
+apt install plasma-widgets-addons kubuntu-wallpapers-bionic -y || true
+apt install latte-dock -y || true
+apt update && sudo apt upgrade -y || true
+rm /var/lib/dpkg/info/rsyslog.postinst; dpkg --configure -a
