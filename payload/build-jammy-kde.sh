14b1ca2fc7051c04e5c9a608ca8ffb052ccf5daf
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-01-23 13:39:39 +0530

    use sudo while updating sources lists

--- payload/build-jammy-kde.sh
+++ payload/build-jammy-kde.sh
@@ -15,7 +15,7 @@ ENABLE_USER_SETUP=false
 PREFIX="${frn}"
 
 additional_setup() {
-cat <<-  EOF > $chroot_dir/etc/apt/sources.list
+$SUDO cat <<  EOF > $chroot_dir/etc/apt/sources.list
 # See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
 # newer versions of the distribution.
 deb $MIRROR $SUITE main restricted
